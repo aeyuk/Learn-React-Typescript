70eb626aeb181bcf7d921ecefcfa2da041c154de
Cate <aeyuk@crimson.ua.edu>
2021-10-13 09:27:13 -0500

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+    <div>
+        <h1>Hello World!</h1>
+    </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+// test commit
