17fe302f0b14c1af6df8e93d9559b9168c3d4b7b
Cate <aeyuk@crimson.ua.edu>
2021-10-15 11:54:32 -0500

    [2.9] Add annotations with children

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,13 +1,19 @@
 interface ChildProps {
     color: string;
+    onClick: () => void;
 }
 
 /* Typescript doesn't know that this is specifically a React function component.
 This method is fine, so long as you don't need access to propTypes, contextTypes,
 defaultProps, and displayName.
 */
-export const Child = ({ color }: ChildProps ) => {
-    return <div>Child: {color}</div>
+export const Child = ({ color, onClick }: ChildProps) => {
+    return (
+        <div>
+            Child: {color}
+            <button color="red" onClick={onClick}>Click me</button>
+        </div>
+    );
 };
 
 /* Another way of defining a function component with Typescript.
@@ -15,8 +21,6 @@ This way tells Typescript that this is specifically a React function componenet,
 and that 'Child' might have properties assigned to it like 'propTypes' and 'contextTypes'.
 'Child' will receive props of type ChildProps
 */
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-    return <div>{color}</div>
+export const ChildAsFc: React.FC<ChildProps> = ({ color, onClick, children }) => {
+    return ( <div>{color}</div> );
 }
-
-ChildAsFC.displayName
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,14 @@
-import { Child } from './Child';
+import { ChildAsFc } from './Child';
+/*  Importing ChildAsFc instead of Child allows Child to be able to directly receive children
+    as props, rather than only taking color and onClick from the parent.
+*/
 
 const Parent = () => {
-    return <Child color="Orange"/>
+    return (
+        <ChildAsFc color="Orange" onClick={() => {console.log("Clicked")}}>
+        
+        </ChildAsFc>
+    );
 };
 
 export default Parent;
